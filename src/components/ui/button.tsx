01a1b268bf2f23d21422608462e2d1464fc81c80
Eryk Sarzynski <eryk.s@rsstudio.net>
2026-02-06 12:16:48 +0100

    update

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -25,7 +25,7 @@ const buttonVariants = cva(
         xs: "h-6 gap-1 rounded-md px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5 text-sm",
         lg: "h-10 rounded-md px-6 py-2 has-[>svg]:px-4 text-lg gap-2",
-        xl: "h-20 rounded-md px-6 py-2 has-[>svg]:px-4 text-lg",
+        xl: "h-16 rounded-md px-6 py-2 has-[>svg]:px-4 text-lg",
         icon: "size-9",
         "icon-xs": "size-6 rounded-md [&_svg:not([class*='size-'])]:size-3",
         "icon-sm": "size-8",
